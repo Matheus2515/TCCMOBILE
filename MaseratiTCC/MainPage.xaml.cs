f92e233044eade26fcf790e491135823c5c34ae6
Matheus <matheus@gmail.com>
2023-10-25 02:23:18 -0300

    first

---
+++ MaseratiTCC/MainPage.xaml.cs
@@ -0,0 +1,21 @@
+﻿namespace MaseratiTCC;
+
+public partial class MainPage : ContentPage
+{
+
+	public MainPage()
+	{
+		InitializeComponent();
+    }
+
+    private void OnNextButtonClicked(object sender, EventArgs e)
+    {
+
+    }
+
+    private void OnPreviousButtonClicked(object sender, EventArgs e)
+    {
+
+    }
+}
+
